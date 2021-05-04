7cde812038431d896062444488b2f8a2398439a5
Pawel <pawel@sliide.com>
2021-05-04 14:11:20 +0100

    NEWS-15 first part of the feature

--- MyProject/app/src/main/java/com/cama/app/myapplication/SettingsActivity.kt
+++ MyProject/app/src/main/java/com/cama/app/myapplication/SettingsActivity.kt
@@ -7,5 +7,7 @@ class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
+
+        // I started first part of the feature
     }
 }
